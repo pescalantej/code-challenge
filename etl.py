c858d32f998e305cec8bb6a6c9f9822212223c08
Pablo Escalante <pescalantej@gmail.com>
2023-10-17 07:34:53 -0300

    updating some comments

--- etl.py
+++ etl.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-# lista con todos los formatos asociados a valores faltantes
+# list of possible missing values names
 missing_value_formats = ["n.a.","?","NA","n/a", "na", "--","..."]
 
 df = pd.read_csv("empleados.csv", na_values = missing_value_formats)
+
+# deleting rows with at least one missing value
 df = df.dropna(axis=0)
 
 print(df.shape)
